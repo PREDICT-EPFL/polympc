786b6d65cf37f2f7fcd990d46be5f8dc41230366
Michael Spieler <michael.spieler@epfl.ch>
2019-04-05 22:03:27 +0200

    NMPC: New constraint hanlding, better init, correct sign errors

--- src/control/nmpc.hpp
+++ src/control/nmpc.hpp
@@ -27,28 +27,33 @@ public:
 
     enum
     {
-        NX_ = State::RowsAtCompileTime,
+        NX = State::RowsAtCompileTime,
         NU = Control::RowsAtCompileTime,
         NP = Parameters::RowsAtCompileTime,
         VARX_SIZE = ode_colloc_t::VARX_SIZE,
         VARU_SIZE = ode_colloc_t::VARU_SIZE,
         VARP_SIZE = ode_colloc_t::VARP_SIZE,
+        VAR_SIZE = var_t::RowsAtCompileTime,
 
-        NX = var_t::RowsAtCompileTime,
-        NIEQ = 2*(VARX_SIZE - NX_ + VARU_SIZE),
-        NEQ = VARX_SIZE + NX_,
-        NC = NIEQ+NEQ
+        NUM_EQ = VARX_SIZE + NX, // ode_collocation + x0
+        NUM_INEQ = 0,
+        NUM_BOX = VARX_SIZE - NX + VARU_SIZE, // X without x0 and U
+        NUM_CONSTR  = NUM_EQ + NUM_INEQ + NUM_BOX,
     };
 
-    // using ode_constr_jac_t = Eigen::Matrix<Scalar, VARX_SIZE, NX>;
-    using ode_constr_jac_t = typename ode_colloc_t::jacobian_t;
-    using sqp_constr_jac_t = Eigen::Matrix<Scalar, NC, NX>;
+    using ode_jacobian_t = typename ode_colloc_t::jacobian_t;
     using cost_gradient_t = var_t;
     using varx_t = typename Eigen::Matrix<Scalar, VARX_SIZE, 1>;
     using varu_t = typename Eigen::Matrix<Scalar, VARU_SIZE, 1>;
 
     using sqp_t = sqp::SQP<nmpc>;
-    using sqp_constraint_t = typename sqp_t::constraint_t;
+
+    using b_eq_t = Eigen::Matrix<Scalar, NUM_EQ, 1>;
+    using A_eq_t = Eigen::Matrix<Scalar, NUM_EQ, VAR_SIZE>;
+    using b_ineq_t = Eigen::Matrix<Scalar, NUM_INEQ, 1>;
+    using A_ineq_t = Eigen::Matrix<Scalar, NUM_INEQ, VAR_SIZE>;
+    using b_box_t = Eigen::Matrix<Scalar, NUM_BOX, 1>;
+    using A_box_t = Eigen::Matrix<Scalar, NUM_BOX, VAR_SIZE>;
 
     cost_colloc_t cost_f;
     ode_colloc_t ps_ode;
@@ -57,117 +62,80 @@ public:
     State _x0;
     State _constr_xl, _constr_xu;
     Control _constr_ul, _constr_uu;
+
     /*
-    var_t var = [xn, ..., x0, un, ..., u0, p]
-    NumSegments = 2
-    POLY_ORDER = 3
-    NUM_NODES = 4
-    VARX_SIZE = (NumSegments * POLY_ORDER + 1) * NX = 21
-    VARU_SIZE = (NumSegments * POLY_ORDER + 1) * NU = 14
-    VARP_SIZE = NP = 1
-    NX = 3
-    NU = 2
-    NP = 1
-
-    xl < x < xu
-    ul < u < uu
-    x0 = x(t0)
-    ps_ode(X) = 0
-
-    translate to:
-
-    for x1-xn:
-    -x + xl < 0
-     x - xu < 0
-    -u + ul < 0
-     u - uu < 0
-    x0 - x(t0) = 0
-    ps_ode(X) = 0
-
-    */
+     * var_t var = [xn, ..., x0, un, ..., u0, p]
+     *
+     * constraints:
+     * xl < x < xu
+     * ul < u < uu
+     * x0 - x(t0) = 0
+     * ps_ode(X) = 0
+     */
 
     void cost(const var_t& var, Scalar &cst)
     {
         cost_f(var, cst);
     }
 
-    void cost_gradient(const var_t& var, cost_gradient_t &grad)
+    void cost_linearized(const var_t& var, cost_gradient_t &grad, Scalar &cst)
     {
-        Scalar cost;
-        cost_f.value_gradient(var, cost, grad);
+        cost_f.value_gradient(var, cst, grad);
     }
 
-    void constraint(const var_t& var, sqp_constraint_t &b) {
+    void constraint(const var_t& var, b_eq_t& b_eq, b_ineq_t& b_ineq, b_box_t& b_box, b_box_t& l_box, b_box_t& u_box) {
         varx_t c_ode;
         ps_ode(var, c_ode);
-        _constraint(var, b, c_ode);
+        _constraint(var, c_ode, b_eq, b_ineq, b_box, l_box, u_box);
     }
 
-    void _constraint(const var_t& var, sqp_constraint_t &b, const varx_t& c_ode)
+    void _constraint(const var_t& var, const varx_t& c_ode, b_eq_t& b_eq, b_ineq_t& b_ineq, b_box_t& b_box, b_box_t& l_box, b_box_t& u_box)
     {
-        unsigned int i = 0;
-        Eigen::Matrix<Scalar, VARX_SIZE - NX_, 1> x;
-        x = var.template head<VARX_SIZE - NX_>();
-        for (unsigned int j = 0; j < VARX_SIZE - NX_; j++) {
-            b.template segment<NX_>(i+j) = -x.template segment<NX_>(j) + _constr_xl; // lower bound
-        }
-        i += VARX_SIZE - NX_;
-        for (unsigned int j = 0; j < VARX_SIZE - NX_; j++) {
-            b.template segment<NX_>(i+j) = x.template segment<NX_>(j) - _constr_xu; // upper bound
-        }
-        i += VARX_SIZE - NX_;
-
-        varu_t u = var.template segment<VARU_SIZE>(VARX_SIZE);
-        for (unsigned int j = 0; j < VARU_SIZE; j++) {
-            b.template segment<NU>(i+j) = -u.template segment<NU>(j) + _constr_ul; // lower bound
-        }
-        i += VARU_SIZE;
-        for (unsigned int j = 0; j < VARU_SIZE; j++) {
-            b.template segment<NU>(i+j) = u.template segment<NU>(j) - _constr_uu; // upper bound
-        }
-        i += VARU_SIZE;
-
-        b.template segment<NX_>(i) = _x0 - var.template head<NX_>(VARX_SIZE - NX_);
-        i += NX_;
-
-        b.template segment<VARX_SIZE>(i) = c_ode;
+        l_box << _constr_xl.template replicate<VARX_SIZE/NX-1, 1>(), _constr_xl.template replicate<VARU_SIZE/NU, 1>();
+        u_box << _constr_xu.template replicate<VARX_SIZE/NX-1, 1>(), _constr_xu.template replicate<VARU_SIZE/NU, 1>();
+        b_box << var.template head<VARX_SIZE+VARU_SIZE>();
+        b_eq << c_ode, var.template segment<NX>(VARX_SIZE - NX) - _x0;
     }
 
-    void constraint_linearized(const var_t& var, sqp_constr_jac_t &A, sqp_constraint_t &b)
+    void constraint_linearized(const var_t& var,
+                               A_eq_t& A_eq,
+                               b_eq_t& b_eq,
+                               A_ineq_t& A_ineq,
+                               b_ineq_t& b_ineq,
+                               A_box_t& A_box,
+                               b_box_t& b_box,
+                               b_box_t& l_box,
+                               b_box_t& u_box)
     {
-        A.setZero();
-        unsigned int row = 0;
-        A.template block<VARX_SIZE-NX_, VARX_SIZE-NX_>(row,0).setIdentity();
-        row += VARX_SIZE-NX_;
-        A.template block<VARX_SIZE-NX_, VARX_SIZE-NX_>(row,0).setIdentity();
-        row += VARX_SIZE-NX_;
-        A.template block<VARU_SIZE, VARU_SIZE>(row,VARX_SIZE).setIdentity();
-        row += VARU_SIZE;
-        A.template block<VARU_SIZE, VARU_SIZE>(row,VARX_SIZE).setIdentity();
-        row += VARU_SIZE;
-        A.template block<NX_, NX_>(row,VARX_SIZE-NX_).setIdentity();
-        row += NX_;
-
+        // TODO: avoid stack allocation
         varx_t c_ode;
-        ode_constr_jac_t ode_jac;
-        ps_ode.linearized(var, ode_jac, c_ode);
-        A.template bottomRows<ode_constr_jac_t::RowsAtCompileTime>() = ode_jac;
+        ode_jacobian_t ode_jac;
 
-        _constraint(var, b, c_ode);
+        ps_ode.linearized(var, ode_jac, c_ode);
+        _constraint(var, c_ode, b_eq, b_ineq, b_box, l_box, u_box);
+
+        A_eq.setZero();
+        A_eq.template bottomRows<VARX_SIZE>() = ode_jac;
+        A_eq.template topRows<NX>().setIdentity();
+
+        A_box.setZero();
+        A_box.template block<VARX_SIZE-NX, VARX_SIZE-NX>(0,0).setIdentity();
+        A_box.template block<VARU_SIZE, VARU_SIZE>(VARX_SIZE-NX,VARX_SIZE).setIdentity();
     }
 
     var_t solve(const State &x0)
     {
         _x0 = x0;
         // TODO: parametrize
-        _constr_xu << 10, 10, 1e17;
+        _constr_xu << 10, 10, 1e20;
         _constr_xl = -_constr_xu;
         _constr_uu << 10, 1;
-        _constr_ul << 0, -1;
+        _constr_ul << -10, -1;
         var_t var0;
-        var0.setOnes();
-        // var0.setZero();
-        var0.template segment<NX_>(VARX_SIZE-NX_) = x0;
+        var0.setZero();
+        // var0.template segment<NX>(VARX_SIZE-NX) = x0;
+        var0.template segment<VARX_SIZE>(0) = x0.template replicate<VARX_SIZE/NX, 1>();
+        var0.template segment<1>(VARX_SIZE+VARU_SIZE).setConstant(1.0);
         solver.solve(*this, var0);
         return solver._x;
     }
--- src/control/nmpc_test.cpp
+++ src/control/nmpc_test.cpp
@@ -14,12 +14,23 @@ TEST(NMPCTestCase, TestRobotNMPC)
     using controller_t = polympc::nmpc<Problem, Approximation, int>;
     using var_t = controller_t::var_t;
 
+    /*
+    NumSegments = 2
+    POLY_ORDER = 3
+    NUM_NODES = 4
+    VARX_SIZE = (NumSegments * POLY_ORDER + 1) * VAR_SIZE = 21
+    VARU_SIZE = (NumSegments * POLY_ORDER + 1) * NU = 14
+    VARP_SIZE = NP = 1
+    VAR_SIZE = 3
+    NU = 2
+    NP = 1
+    */
+
     controller_t robot_controller;
-    // Eigen::Vector3d x0 = {1, 1, 3.141};
-    Eigen::Vector3d x0 = {1, 1, 0};
+    Eigen::Vector3d x0 = {-1, 0, 0};
+    std::cout << "x0 " << x0.transpose() << std::endl;
 
     var_t sol = robot_controller.solve(x0);
-    std::cout << sol.transpose() << std::endl;
 
     Eigen::IOFormat fmt(Eigen::StreamPrecision, 0, ", ", ",", "[", "]");
     printf("xy\n");
@@ -36,6 +47,9 @@ TEST(NMPCTestCase, TestRobotNMPC)
     for (int i = 0; i < 7; i++) {
        std::cout << sol.segment<2>(21+3*i).transpose().format(fmt) << ",\n";
     }
+
+    Eigen::IOFormat fmt2(2);
+    std::cout << "dual\n" << robot_controller.solver._lambda.transpose().format(fmt2) << std::endl;
 
     printf("controller_t size %lu\n", sizeof(controller_t));
     printf("controller_t::cost_colloc_t size %lu\n", sizeof(controller_t::cost_colloc_t));

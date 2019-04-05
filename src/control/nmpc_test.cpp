75b1fc1762cf634034feaa2451a66e24ffa0c75d
Michael Spieler <michael.spieler@epfl.ch>
2019-04-06 00:13:21 +0200

    NMPC: Debug solution

--- src/control/nmpc_test.cpp
+++ src/control/nmpc_test.cpp
@@ -26,31 +26,6 @@ TEST(NMPCTestCase, TestRobotNMPC)
     NP = 1
     */
 
-    controller_t robot_controller;
-    Eigen::Vector3d x0 = {-1, 0, 0};
-    std::cout << "x0 " << x0.transpose() << std::endl;
-
-    var_t sol = robot_controller.solve(x0);
-
-    Eigen::IOFormat fmt(Eigen::StreamPrecision, 0, ", ", ",", "[", "]");
-    printf("xy\n");
-    for (int i = 0; i < 7; i++) {
-       std::cout << sol.segment<2>(3*i).transpose().format(fmt) << ",\n";
-    }
-
-    printf("theta\n");
-    for (int i = 0; i < 7; i++) {
-       std::cout << sol.segment<1>(3*i+2).transpose().format(fmt) << ",\n";
-    }
-
-    printf("u\n");
-    for (int i = 0; i < 7; i++) {
-       std::cout << sol.segment<2>(21+3*i).transpose().format(fmt) << ",\n";
-    }
-
-    Eigen::IOFormat fmt2(2);
-    std::cout << "dual\n" << robot_controller.solver._lambda.transpose().format(fmt2) << std::endl;
-
     printf("controller_t size %lu\n", sizeof(controller_t));
     printf("controller_t::cost_colloc_t size %lu\n", sizeof(controller_t::cost_colloc_t));
     printf("controller_t::ode_colloc_t size %lu\n", sizeof(controller_t::ode_colloc_t));
@@ -58,22 +33,48 @@ TEST(NMPCTestCase, TestRobotNMPC)
     printf("controller_t::sqp_t::qp_t size %lu\n", sizeof(controller_t::sqp_t::qp_t));
     printf("controller_t::sqp_t::qp_solver_t size %lu\n", sizeof(controller_t::sqp_t::qp_solver_t));
 
+    controller_t robot_controller;
+
     Eigen::Vector3d x0_list[] = {
-        {1, 1, 0},
+        {-1, 0, 0},
         {-1, -1, 0},
-        {1, 0.5, 0},
-        {1, 1, -1.5},
-        {1, 1, 1.5},
+        {-1, -1, 0.7},
+        {-10, -10, 0},
+        {1, 1, 0},
         {10, 10, 0},
+        {0, 1, 0.7},
+        {0, 1, 0},
         {0, 0, 0},
     };
 
     for (auto& x0: x0_list) {
         std::cout << "x0 " << x0.transpose() << std::endl;
         var_t sol = robot_controller.solve(x0);
+
+        Eigen::IOFormat fmt(4, 0, ", ", ",", "[", "]");
+        printf("xy\n");
         for (int i = 0; i < 7; i++) {
            std::cout << sol.segment<2>(3*i).transpose().format(fmt) << ",\n";
         }
+
+        printf("theta\n");
+        for (int i = 0; i < 7; i++) {
+           std::cout << sol.segment<1>(3*i+2).transpose().format(fmt) << ",\n";
+        }
+
+        printf("u\n");
+        for (int i = 0; i < 7; i++) {
+           std::cout << sol.segment<2>(21+2*i).transpose().format(fmt) << ",\n";
+        }
+        printf("p\n");
+        std::cout << sol.tail<1>().format(fmt) << ",\n";
+
+        printf("iter %d\n", robot_controller.solver.iter);
+        printf("dual\n");
+        std::cout << "  ode   " << robot_controller.solver._lambda.template segment<controller_t::VARX_SIZE>(0).transpose().format(fmt) << std::endl;
+        std::cout << "  x0    " << robot_controller.solver._lambda.template segment<controller_t::NX>(controller_t::VARX_SIZE).transpose().format(fmt) << std::endl;
+        std::cout << "  x     " << robot_controller.solver._lambda.template segment<controller_t::VARX_SIZE-controller_t::NX>(controller_t::VARX_SIZE+controller_t::NX).transpose().format(fmt) << std::endl;
+        std::cout << "  u     " << robot_controller.solver._lambda.template segment<controller_t::VARU_SIZE>(2*controller_t::VARX_SIZE).transpose().format(fmt) << std::endl;
     }
 }
 

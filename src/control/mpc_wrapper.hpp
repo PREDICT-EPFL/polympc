cadfe32cc1c7f7e2bd5684840c9bbd0f9c0223e7
Peter <petr.listov@epfl.ch>
2021-02-04 17:08:46 +0100

    MPC wrapper class added

---
+++ src/control/mpc_wrapper.hpp
@@ -0,0 +1,240 @@
+#ifndef MPC_WRAPPER_HPP
+#define MPC_WRAPPER_HPP
+
+#include "utils/helpers.hpp"
+#include "polynomials/splines.hpp"
+#include "polymath.h"
+#include <iostream>
+
+/** interface/aggregate class for MPC controllers */
+template<typename OCP, template<typename, typename ...Args> class Solver, typename ...Args>
+class MPC
+{
+
+private:
+    using nlp_solver_t = Solver<OCP, Args...>;
+    nlp_solver_t m_solver;
+
+    /** dynamic system properties */
+    static constexpr int nx = OCP::NX;
+    static constexpr int nu = OCP::NU;
+    static constexpr int np = OCP::NP;
+    static constexpr int nd = OCP::ND;
+
+    /** optimisation properties */
+    static constexpr int var_size  = OCP::VAR_SIZE;
+    static constexpr int varx_size = OCP::VARX_SIZE;
+    static constexpr int varu_size = OCP::VARU_SIZE;
+    static constexpr int dual_size = OCP::DUAL_SIZE;
+    static constexpr int num_nodes = OCP::NUM_NODES;
+    static constexpr int num_segms = OCP::NUM_SEGMENTS;
+
+public:
+    MPC()
+    {
+        // initialise time nodes and Lagrange interpolator
+        time_grid = this->ocp().time_nodes.reverse();
+        time_nodes = time_grid.head(time_grid.rows());
+        polympc::LagrangeSpline::compute_lagrange_basis(time_nodes, m_basis);
+        sgm_length = (OCP::t_stop - OCP::t_start) / num_segms;
+        nodes_per_segm = std::floor(num_nodes / num_segms);
+
+        /**
+        Eigen::Matrix<scalar_t, 2, 6> values; values << 1, 2, 1, 2, 1, 2,  1, 1, 1, 1, 1, 1;
+
+        auto val = polympc::LagrangeSpline::eval(0.5, values, m_basis);
+
+        Eigen::VectorXd lox;
+
+        std::cout << time_nodes.transpose() << "\n";
+        std::cout << m_basis << "\n";
+        std::cout << val.transpose() << "\n";
+        */
+
+    }
+    ~MPC() = default;
+
+    using scalar_t     = typename OCP::scalar_t;
+    using state_t      = typename dense_matrix_type_selector<scalar_t, nx, 1>::type;
+    using control_t    = typename dense_matrix_type_selector<scalar_t, nu, 1>::type;
+    using parameter_t  = typename dense_matrix_type_selector<scalar_t, np, 1>::type;
+    using static_param = typename dense_matrix_type_selector<scalar_t, nd, 1>::type;
+
+    using traj_state_t   = Eigen::Matrix<scalar_t, varx_size, 1>;
+    using traj_control_t = Eigen::Matrix<scalar_t, varu_size, 1>;
+    using dual_var_t     = Eigen::Matrix<scalar_t, dual_size, 1>;
+
+    /** data for interpolation */
+    typename OCP::time_t  time_grid;
+    typename OCP::nodes_t time_nodes;
+    using interpolation_basis_t = Eigen::Matrix<scalar_t, OCP::nodes_t::RowsAtCompileTime, OCP::nodes_t::RowsAtCompileTime>;
+    interpolation_basis_t m_basis;
+    scalar_t sgm_length{1.0};
+    int nodes_per_segm{1};
+
+    /** set initial conditions bounds */
+    inline void initial_conditions(const Eigen::Ref<const state_t>& x0) noexcept
+    {
+        m_solver.upper_bound_x().template segment<nx>(varx_size - nx) = x0;
+        m_solver.lower_bound_x().template segment<nx>(varx_size - nx) = x0;
+    }
+    inline void initial_conditions(const Eigen::Ref<const state_t>& x0_lb,
+                                   const Eigen::Ref<const state_t>& x0_ub) noexcept
+    {
+        m_solver.upper_bound_x().template segment<nx>(varx_size - nx) = x0_ub;
+        m_solver.lower_bound_x().template segment<nx>(varx_size - nx) = x0_lb;
+    }
+
+    /** set state and control bounds */
+    // state
+    inline void x_lower_bound(const Eigen::Ref<const state_t>& xlb) noexcept
+    {
+        m_solver.lower_bound_x().template head<varx_size - nx>() = xlb.replicate(num_nodes - 1, 1);
+    }
+    inline void x_upper_bound(const Eigen::Ref<const state_t>& xub) noexcept
+    {
+        m_solver.upper_bound_x().template head<varx_size - nx>() = xub.replicate(num_nodes - 1, 1);
+    }
+    inline void state_bounds(const Eigen::Ref<const state_t>& xlb,
+                             const Eigen::Ref<const state_t>& xub) noexcept
+    {
+        m_solver.lower_bound_x().template head<varx_size - nx>() = xlb.replicate(num_nodes - 1, 1);
+        m_solver.upper_bound_x().template head<varx_size - nx>() = xub.replicate(num_nodes - 1, 1);
+    }
+    inline void state_trajectory_bounds(const Eigen::Ref<const traj_state_t>& xlb,
+                                        const Eigen::Ref<const traj_state_t>& xub) noexcept
+    {
+        m_solver.lower_bound_x().template head<varx_size>() = xlb;
+        m_solver.upper_bound_x().template head<varx_size>() = xub;
+    }
+
+    // control
+    inline void u_lower_bound(const Eigen::Ref<const control_t>& lb) noexcept
+    {
+        m_solver.lower_bound_x().template segment<varu_size>(varx_size) = lb.replicate(num_nodes, 1);
+    }
+    inline void u_upper_bound(const Eigen::Ref<const control_t>& ub) noexcept
+    {
+        m_solver.upper_bound_x().template segment<varu_size>(varx_size) = ub.replicate(num_nodes, 1);
+    }
+    inline void control_trajecotry_bounds(const Eigen::Ref<const traj_control_t>& lb,
+                                          const Eigen::Ref<const traj_control_t>& ub) noexcept
+    {
+        m_solver.lower_bound_x().template segment<varu_size>(varx_size) = lb;
+        m_solver.upper_bound_x().template segment<varu_size>(varx_size) = ub;
+    }
+    inline void control_bounds(const Eigen::Ref<const control_t>& lb,
+                               const Eigen::Ref<const control_t>& ub) noexcept
+    {
+        m_solver.lower_bound_x().template segment<varu_size>(varx_size) = lb.replicate(num_nodes, 1);
+        m_solver.upper_bound_x().template segment<varu_size>(varx_size) = ub.replicate(num_nodes, 1);
+    }
+
+    /** set parameters bounds*/
+    inline void parameters_bounds(const Eigen::Ref<const parameter_t>& lbp, const Eigen::Ref<const parameter_t>& ubp) noexcept
+    {
+        m_solver.lower_bound_x().template tail<np>() = lbp;
+        m_solver.upper_bound_x().template tail<np>() = ubp;
+    }
+
+    /** set static parameters */
+    inline void set_static_parameters(const Eigen::Ref<const static_param>& param) noexcept
+    {
+        m_solver.parameters() = param;
+    }
+
+    /** set initial guess */
+    inline void x_guess(const Eigen::Ref<const traj_state_t>& x_guess) noexcept
+    {
+        m_solver.m_x.template head<varx_size>() = x_guess;
+    }
+    inline void u_guess(const Eigen::Ref<const traj_control_t>& u_guess) noexcept
+    {
+        m_solver.m_x.template segment<varu_size>(varx_size) = u_guess;
+    }
+    inline void lam_guess(const Eigen::Ref<const dual_var_t>& lam_guess) noexcept
+    {
+        m_solver.m_lam = lam_guess;
+    }
+
+    /** get and set solver settings | info | problem */
+    EIGEN_STRONG_INLINE const typename nlp_solver_t::nlp_settings_t& settings() const noexcept { return m_solver.m_settings; }
+    EIGEN_STRONG_INLINE typename nlp_solver_t::nlp_settings_t& settings() noexcept { return m_solver.m_settings; }
+
+    EIGEN_STRONG_INLINE const typename nlp_solver_t::nlp_info_t& info() const noexcept { return m_solver.m_info; }
+    EIGEN_STRONG_INLINE typename nlp_solver_t::nlp_info_t& info() noexcept { return m_solver.m_info; }
+
+    EIGEN_STRONG_INLINE const OCP& ocp() const noexcept { return m_solver.get_problem(); }
+    EIGEN_STRONG_INLINE OCP& ocp() noexcept { return m_solver.get_problem(); }
+
+    /** get the solution */
+    // state
+    inline traj_state_t solution_x() const noexcept
+    {
+        return m_solver.primal_solution().template head<varx_size>();
+    }
+    inline Eigen::Matrix<scalar_t, nx, num_nodes> solution_x_reshaped() const noexcept
+    {
+        traj_state_t opt_x = m_solver.primal_solution().template head<varx_size>();
+        return Eigen::Map<Eigen::Matrix<scalar_t, nx, num_nodes>>(opt_x.data(), nx, num_nodes).rowwise().reverse();;
+    }
+    inline state_t solution_x_at(const int &k) const noexcept
+    {
+        return m_solver.primal_solution().template segment<nx>(varx_size - (k + 1) * nx);
+    }
+    inline state_t solution_x_at(const scalar_t& t) const noexcept
+    {
+        auto x_reshaped = solution_x_reshaped();
+        Eigen::Index idx = (Eigen::Index)std::floor( t / sgm_length);
+        idx = std::max(Eigen::Index(0), std::min(idx, (Eigen::Index)num_segms - 1)); // clip idx to stay within the spline bounds
+
+        state_t state = polympc::LagrangeSpline::eval(t, x_reshaped.template block<nx,
+                                                      OCP::nodes_t::RowsAtCompileTime>(0, idx * nodes_per_segm), m_basis);
+
+        return state;
+    }
+
+    // control
+    inline traj_control_t solution_u() const noexcept
+    {
+        return m_solver.primal_solution().template segment<varu_size>(varx_size);
+    }
+    inline Eigen::Matrix<scalar_t, nu, num_nodes> solution_u_reshaped() const noexcept
+    {
+        traj_control_t opt_u = m_solver.primal_solution().template segment<varu_size>(varx_size);
+        return Eigen::Map<Eigen::Matrix<scalar_t, nu, num_nodes>>(opt_u.data(), nu, num_nodes).rowwise().reverse();
+    }
+    inline control_t solution_u_at(const int &k) const noexcept
+    {
+        return m_solver.primal_solution().template segment<nu>(varx_size + varu_size - (k + 1) * nu);
+    }
+    inline control_t solution_u_at(const scalar_t& t) const noexcept
+    {
+        auto u_reshaped = solution_u_reshaped();
+        Eigen::Index idx = (Eigen::Index)std::floor( t / sgm_length);
+        idx = std::max(Eigen::Index(0), std::min(idx, (Eigen::Index)num_segms - 1)); // clip idx to stay within the spline bounds
+
+        control_t state = polympc::LagrangeSpline::eval(t, u_reshaped.template block<nu,
+                                                      OCP::nodes_t::RowsAtCompileTime>(0, idx * nodes_per_segm), m_basis);
+
+        return state;
+    }
+
+
+    // parameters
+    inline parameter_t solution_p() const noexcept
+    {
+        return m_solver.primal_solution().template tail<np>();
+    }
+
+    /** solve optimal control problem */
+    inline void solve() noexcept {m_solver.solve();}
+
+};
+
+
+
+
+
+
+#endif // MPC_WRAPPER_HPP
---
+++ tests/control/mpc_wrapper_test.cpp
@@ -0,0 +1,233 @@
+#include "solvers/sqp_base.hpp"
+#include "polynomials/ebyshev.hpp"
+#include "control/continuous_ocp.hpp"
+#include "control/mpc_wrapper.hpp"
+#include "polynomials/splines.hpp"
+
+#include <iomanip>
+#include <iostream>
+#include <chrono>
+
+#include "control/simple_robot_model.hpp"
+#include "solvers/box_admm.hpp"
+#include "solvers/admm.hpp"
+#include "solvers/qp_preconditioners.hpp"
+
+#define test_POLY_ORDER 5
+#define test_NUM_SEG    2
+#define test_NUM_EXP    1
+
+/** benchmark the new collocation class */
+using Polynomial = polympc::Chebyshev<test_POLY_ORDER, polympc::GAUSS_LOBATTO, double>;
+using Approximation = polympc::Spline<Polynomial, test_NUM_SEG>;
+
+POLYMPC_FORWARD_DECLARATION(/*Name*/ RobotOCP, /*NX*/ 3, /*NU*/ 2, /*NP*/ 0, /*ND*/ 1, /*NG*/0, /*TYPE*/ double)
+
+using namespace Eigen;
+
+class RobotOCP : public ContinuousOCP<RobotOCP, Approximation, SPARSE>
+{
+public:
+    ~RobotOCP() = default;
+
+    static constexpr double t_start = 0.0;
+    static constexpr double t_stop  = 2.0;
+
+    Eigen::DiagonalMatrix<scalar_t, 3> Q{1,1,1};
+    Eigen::DiagonalMatrix<scalar_t, 2> R{1,1};
+    Eigen::DiagonalMatrix<scalar_t, 3> QN{1,1,1};
+
+    template<typename T>
+    inline void dynamics_impl(const Eigen::Ref<const state_t<T>> x, const Eigen::Ref<const control_t<T>> u,
+                              const Eigen::Ref<const parameter_t<T>> p, const Eigen::Ref<const static_parameter_t> &d,
+                              const T &t, Eigen::Ref<state_t<T>> xdot) const noexcept
+    {
+        xdot(0) = u(0) * cos(x(2)) * cos(u(1));
+        xdot(1) = u(0) * sin(x(2)) * cos(u(1));
+        xdot(2) = u(0) * sin(u(1)) / d(0);
+    }
+
+    template<typename T>
+    inline void lagrange_term_impl(const Eigen::Ref<const state_t<T>> x, const Eigen::Ref<const control_t<T>> u,
+                                   const Eigen::Ref<const parameter_t<T>> p, const Eigen::Ref<const static_parameter_t> d,
+                                   const scalar_t &t, T &lagrange) noexcept
+    {
+        Eigen::Matrix<T,3,3> Qm = Q.toDenseMatrix().template cast<T>();
+        Eigen::Matrix<T,2,2> Rm = R.toDenseMatrix().template cast<T>();
+
+        lagrange = x.dot(Qm * x) + u.dot(Rm * u);
+    }
+
+    template<typename T>
+    inline void mayer_term_impl(const Eigen::Ref<const state_t<T>> x, const Eigen::Ref<const control_t<T>> u,
+                                const Eigen::Ref<const parameter_t<T>> p, const Eigen::Ref<const static_parameter_t> d,
+                                const scalar_t &t, T &mayer) noexcept
+    {
+        Eigen::Matrix<T,3,3> Qm = Q.toDenseMatrix().template cast<T>();
+        mayer = x.dot(Qm * x);
+    }
+
+    void set_Q_coeff(const scalar_t& coeff)
+    {
+        Q.diagonal() << coeff, coeff, coeff;
+    }
+};
+
+/** create solver */
+template<typename Problem, typename QPSolver> class MySolver;
+
+template<typename Problem, typename QPSolver = boxADMM<Problem::VAR_SIZE, Problem::DUAL_SIZE, typename Problem::scalar_t>>
+class MySolver : public SQPBase<MySolver<Problem, QPSolver>, Problem, QPSolver>
+{
+public:
+    using Base = SQPBase<MySolver<Problem, QPSolver>, Problem, QPSolver>;
+    using typename Base::scalar_t;
+    using typename Base::nlp_variable_t;
+    using typename Base::nlp_hessian_t;
+
+    //EIGEN_STRONG_INLINE const typename Base::_Problem& get_problem() const noexcept { return this->problem; }
+    EIGEN_STRONG_INLINE Problem& get_problem() noexcept { return this->problem; }
+
+    /** change step size selection algorithm */
+    scalar_t step_size_selection_impl(const Ref<const nlp_variable_t>& p) noexcept
+    {
+        //std::cout << "taking NEW implementation \n";
+        scalar_t mu, phi_l1, Dp_phi_l1;
+        nlp_variable_t cost_gradient = this->m_h;
+        const scalar_t tau = this->m_settings.tau; // line search step decrease, 0 < tau < settings.tau
+
+        scalar_t constr_l1 = this->constraints_violation(this->m_x);
+
+        // TODO: get mu from merit function model using hessian of Lagrangian
+        //const scalar_t quad_term = p.dot(this->m_H * p);
+        //const scalar_t qt = quad_term >= 0 ? scalar_t(0.5) * quad_term : 0;
+        //mu = (abs(cost_gradient.dot(p)) ) / ((1 - this->m_settings.rho) * constr_l1);
+
+        mu = this->m_lam_k.template lpNorm<Eigen::Infinity>();
+
+        //std::cout << "mu: " << mu << "\n";
+
+        scalar_t cost_1;
+        this->problem.cost(this->m_x, this->m_p, cost_1);
+
+        //std::cout << "l1: " << constr_l1 << " cost: " << cost_1 << "\n";
+
+        phi_l1 = cost_1 + mu * constr_l1;
+        Dp_phi_l1 = cost_gradient.dot(p) - mu * constr_l1;
+
+        scalar_t alpha = scalar_t(1.0);
+        scalar_t cost_step;
+        nlp_variable_t x_step;
+        for (int i = 1; i < this->m_settings.line_search_max_iter; i++)
+        {
+            x_step.noalias() = alpha * p;
+            x_step += this->m_x;
+            this->problem.cost(x_step, this->m_p, cost_step);
+
+            //std::cout << "i: " << i << " l1: " << this->constraints_violation(x_step) << " cost: " << cost_step << "\n";
+
+            scalar_t phi_l1_step = cost_step + mu * this->constraints_violation(x_step);
+
+            //std::cout << "phi before: " << phi_l1 << " after: " << phi_l1_step <<  " required diff: " << alpha * this->m_settings.eta * Dp_phi_l1 << "\n";
+
+            if (phi_l1_step <= (phi_l1 + alpha * this->m_settings.eta * Dp_phi_l1))
+            {
+                // accept step
+                return alpha;
+            } else {
+                alpha = tau * alpha;
+            }
+        }
+
+        return alpha;
+    }
+
+    /** change Hessian update algorithm to the one provided by ContinuousOCP*/
+    EIGEN_STRONG_INLINE void hessian_update_impl(Eigen::Ref<nlp_hessian_t> hessian, const Eigen::Ref<const nlp_variable_t>& x_step,
+                                                 const Eigen::Ref<const nlp_variable_t>& grad_step) noexcept
+    {
+        this->problem.hessian_update_impl(hessian, x_step, grad_step);
+    }
+};
+
+
+/** QP solvers */
+using admm_solver = ADMM<RobotOCP::VAR_SIZE, RobotOCP::NUM_EQ, RobotOCP::scalar_t,
+                         RobotOCP::MATRIXFMT, linear_solver_traits<RobotOCP::MATRIXFMT>::default_solver>;
+
+using box_admm_solver = boxADMM<RobotOCP::VAR_SIZE, RobotOCP::NUM_EQ, RobotOCP::scalar_t,
+                                RobotOCP::MATRIXFMT, linear_solver_traits<RobotOCP::MATRIXFMT>::default_solver>;
+
+using preconditioner_t = RuizEquilibration<RobotOCP::scalar_t, RobotOCP::VAR_SIZE, RobotOCP::NUM_EQ, RobotOCP::MATRIXFMT>;
+
+
+int main(void)
+{
+    using mpc_t = MPC<RobotOCP, MySolver, box_admm_solver>;
+    mpc_t mpc;
+    mpc.ocp().set_Q_coeff(2.0);
+    mpc.settings().max_iter = 20;
+    mpc.settings().line_search_max_iter = 10;
+    // problem data
+    mpc_t::static_param p; p << 2.0;          // robot wheel base
+    mpc_t::state_t x0; x0 << 0.5, 0.5, 0.5;   // initial condition
+    mpc_t::control_t lbu; lbu << -1.5, -0.75; // lower bound on control
+    mpc_t::control_t ubu; ubu <<  1.5,  0.75; // upper bound on control
+
+    mpc.set_static_parameters(p);
+    mpc.control_bounds(lbu, ubu);
+    mpc.initial_conditions(x0);
+
+    polympc::time_point start = polympc::get_time();
+    mpc.solve();
+    polympc::time_point stop = polympc::get_time();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "MPC status: " << mpc.info().status.value << "\n";
+    std::cout << "Num iterations: " << mpc.info().iter << "\n";
+    std::cout << "Solve time: " << std::setprecision(9) << static_cast<double>(duration.count()) << "[mc] \n";
+
+    std::cout << "Solution X: " << mpc.solution_x().transpose() << "\n";
+    std::cout << "Solution U: " << mpc.solution_u().transpose() << "\n";
+
+    // warm started iteration
+    x0 << 0.3, 0.4, 0.5;
+    mpc.initial_conditions(x0, x0);
+
+    start = polympc::get_time();
+    mpc.solve();
+    stop = polympc::get_time();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Solve status: " << mpc.info().status.value << "\n";
+    std::cout << "Num iterations: " << mpc.info().iter << "\n";
+    std::cout << "Solve time: " << std::setprecision(9) << static_cast<double>(duration.count()) << "[mc] \n";
+
+    std::cout << "Solution X: \n" << mpc.solution_x_reshaped() << "\n";
+    std::cout << "Solution U: \n" << mpc.solution_u_reshaped() << "\n";
+
+    // sample x solution at collocation points [0, 5, 10]
+    std::cout << "x[0]: " << mpc.solution_x_at(0).transpose() << "\n";
+    std::cout << "x[5]: " << mpc.solution_x_at(5).transpose() << "\n";
+    std::cout << "x[10]: " << mpc.solution_x_at(10).transpose() << "\n";
+
+    std::cout << " ------------------------------------------------ \n";
+
+    //sample control at collocation points
+    std::cout << "u[0]: " << mpc.solution_u_at(0).transpose() << "\n";
+    std::cout << "u[1]: " << mpc.solution_u_at(1).transpose() << "\n";
+
+    std::cout << " ------------------------------------------------ \n";
+
+    // sample state at time 't'
+    std::cout << "x(0.0): " << mpc.solution_x_at(0.0).transpose() << "\n";
+    std::cout << "x(0.5): " << mpc.solution_x_at(0.5).transpose() << "\n";
+
+    std::cout << " ------------------------------------------------ \n";
+
+    //  sample control at time 't'
+    std::cout << "u(0.0): " << mpc.solution_u_at(0.0).transpose() << "\n";
+    std::cout << "u(0.5): " << mpc.solution_u_at(0.5).transpose() << "\n";
+
+    return EXIT_SUCCESS;
+}
